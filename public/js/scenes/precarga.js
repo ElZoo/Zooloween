030ebeb31ab41e72a4c3f149d65ffae05aac361a
chistian95 <kriskorral59@gmail.com>
2018-10-04 20:52:03 +0200

    Crear mapa básico y cámara

--- public/js/scenes/precarga.js
+++ public/js/scenes/precarga.js
@@ -3,14 +3,14 @@ var scenePrecarga = {
   active: true,
 
   init: function() {
-
+    this.socket = io();
   },
 
   preload: function() {
-
+    this.load.atlas('dungeon', 'res/dungeon.png', 'res/dungeon.json');
   },
 
   create: function() {
-    this.socket = io();
+    this.scene.launch('principal');
   },
 }
