3cae10f80d1ef8db0210f497e953c24136a20052
chistian95 <kriskorral59@gmail.com>
2018-10-06 15:58:43 +0200

    Añadir murcielagos con IA básica

--- public/js/scenes/precarga.js
+++ public/js/scenes/precarga.js
@@ -8,8 +8,8 @@ var scenePrecarga = {
 
   preload: function() {
     this.load.atlas('dungeon', 'res/dungeon.png', 'res/dungeon.json');
-
     this.load.atlas('pj_base', 'res/pj_base.png', 'res/pj_base.json');
+    this.load.atlas('murcielago', 'res/murcielago.png', 'res/murcielago.json');
   },
 
   create: function() {
@@ -19,8 +19,9 @@ var scenePrecarga = {
     this.game.datos.socket = io();
     this.game.datos.socket.on('datosMapa', function(datos) {
       self.game.datos.jugadores = datos[0];
-      self.game.datos.tiles_mundo = datos[1];
-      self.game.datos.items_mundo = datos[2];
+      self.game.datos.mobs = datos[1];
+      self.game.datos.tiles_mundo = datos[2];
+      self.game.datos.items_mundo = datos[3];
 
       self.scene.launch('principal');
     });
@@ -30,5 +31,8 @@ var scenePrecarga = {
     this.anims.create({key: 'pj_abajo', frames: this.anims.generateFrameNames('pj_base', {prefix: 'abajo_', end: 3}), repeat: -1, frameRate: 4});
     this.anims.create({key: 'pj_izquierda', frames: this.anims.generateFrameNames('pj_base', {prefix: 'izquierda_', end: 3}), repeat: -1, frameRate: 4});
     this.anims.create({key: 'pj_derecha', frames: this.anims.generateFrameNames('pj_base', {prefix: 'derecha_', end: 3}), repeat: -1, frameRate: 4});
+
+    this.anims.create({key: 'murcielago_volar', frames: this.anims.generateFrameNames('murcielago', {prefix: 'volar_', end: 4}), repeat: -1, frameRate: 8});
+    this.anims.create({key: 'murcielago_morir', frames: this.anims.generateFrameNames('murcielago', {prefix: 'morir_', end: 4}), repeat: 0, frameRate: 8});
   },
 }
